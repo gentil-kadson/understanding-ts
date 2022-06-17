ed288817e12c583a204590882cd628cbb6714c41
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2022-06-17 14:19:41 -0300

    finished TypeScript Basics

--- unkownAndNever.ts
+++ unkownAndNever.ts
@@ -1,13 +1,13 @@
 let userInput: unknown; // unknown type can't be assigned to a value other than unkown, because it can change later in the code.
 // unknown is better than any because you have a type check to guarantee it's value, if assigned to a type other than unkown, won't change along the way and cause trouble as any might do.
-let userName: string;
+// let userName: string;
 
-userInput = 20;
-userInput = "Kadson";
+// userInput = 20;
+// userInput = "Kadson";
 
-if (typeof userInput === "string") {
-  userName = userInput;
-}
+// if (typeof userInput === "string") {
+//   userName = userInput;
+// }
 
 function generateError(message: string, code: number): never { // never specificies that it will NEVER return something, either because the script (or this part of the script) crashed or because of an infinite loop.
   throw { message: message, errorCode: code };
